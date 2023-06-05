52639a1830d41e3cece254a0be9cb21086054298
Erik Deheza <135668315+ErikDeheza@users.noreply.github.com>
2023-06-05 19:47:59 -0300

    Update main.cpp

---
+++ 01-CalcularPi/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    double pi = 0.0;
+    double term = 1.0;
+    double denominator = 1.0;
+    int sign = 1;
+
+    while (pi < 3.141592 || pi > 3.141593) {
+        pi += sign * (4.0 / denominator);
+        denominator += 2.0;
+        sign *= -1;
+    }
+
+    std::cout << "El valor aproximado de π es: " << pi << std::endl;
+
+    return 0;
+}
